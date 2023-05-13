40f2456f64d1013f352c6cfe9463722b97933cd9
Tuyet Nguyen <tnus1226@gmail.com>
2023-05-12 23:33:34 -0700

    init commit

--- src/assets/configs/homeConfig.js
+++ src/assets/configs/homeConfig.js
@@ -8,14 +8,10 @@ const homeConfig = {
 
     greeting:
         <h1 className="heading">
-            Hi! I'm <strong className="main-name"> Rodrigo Arenas</strong>
+            Hi! I'm <strong className="main-name"> Tuyet Nguyen</strong>
         </h1>,
     titles: [
-        "Data Scientist",
-        "Machine Learning Specialist",
-        "Open Source Contributor",
-        "Backend Developer",
-        "Colombian"
+        "Data Analyst"
     ],
     about: {
         start: "I've been working for over six years on data science projects. " +
