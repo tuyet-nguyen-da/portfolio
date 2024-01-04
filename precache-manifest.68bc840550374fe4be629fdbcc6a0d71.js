c317803be0369c0d1856ed8a4e4ca1e658f69327
tuyet-nguyen-da <tnus1226@gmail.com>
2024-01-03 22:23:16 -0800

    Updates

---
+++ precache-manifest.68bc840550374fe4be629fdbcc6a0d71.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = [
+  {
+    "revision": "4325cd86e3ded286e1b4",
+    "url": "/portfolio/static/css/main.39cb20da.chunk.css"
+  },
+  {
+    "revision": "4325cd86e3ded286e1b4",
+    "url": "/portfolio/static/js/main.4325cd86.chunk.js"
+  },
+  {
+    "revision": "44e8f9feefc6b0928670",
+    "url": "/portfolio/static/css/1.80d32188.chunk.css"
+  },
+  {
+    "revision": "44e8f9feefc6b0928670",
+    "url": "/portfolio/static/js/1.44e8f9fe.chunk.js"
+  },
+  {
+    "revision": "ee2a100325fd284f0097",
+    "url": "/portfolio/static/js/runtime~main.ee2a1003.js"
+  },
+  {
+    "revision": "23273bc8bc3197e29e433d7cf66185fc",
+    "url": "/portfolio/static/media/sklearn_genetic_opt.23273bc8.png"
+  },
+  {
+    "revision": "6be2b60d9b093a4f964efff43176fe41",
+    "url": "/portfolio/static/media/influenza.6be2b60d.webp"
+  },
+  {
+    "revision": "14dfa9afbf2e12bc6cc156bef290f3f3",
+    "url": "/portfolio/static/media/rockbuster.14dfa9af.PNG"
+  },
+  {
+    "revision": "4b1919eb8bfc85d8179920a8ea3ca5d3",
+    "url": "/portfolio/static/media/airbnb.4b1919eb.PNG"
+  },
+  {
+    "revision": "3d8f739aeebff2cba46c49fbb7fdbd42",
+    "url": "/portfolio/static/media/instacart.3d8f739a.PNG"
+  },
+  {
+    "revision": "f27502311d64cd53d28d0312a545f37e",
+    "url": "/portfolio/static/media/gameco.f2750231.PNG"
+  },
+  {
+    "revision": "f7d742fc14d3d495da2581043372d15a",
+    "url": "/portfolio/static/media/fastapi_sklearn.f7d742fc.png"
+  },
+  {
+    "revision": "fed0850a36ae8cfbe9ceaa1ce970c5cc",
+    "url": "/portfolio/static/media/graph_embeddings.fed0850a.png"
+  },
+  {
+    "revision": "9c0f986795edbdc96c99bc8853580310",
+    "url": "/portfolio/static/media/kafkaml.9c0f9867.png"
+  },
+  {
+    "revision": "8ba027d94d0cd4d31608e2c6e43f9488",
+    "url": "/portfolio/static/media/scikit_pipes.8ba027d9.png"
+  },
+  {
+    "revision": "4b632d51a45152af9b70077a3d4955ca",
+    "url": "/portfolio/static/media/portfolio.4b632d51.png"
+  },
+  {
+    "revision": "18a2497c3e5cb4a29ba66f0b39284445",
+    "url": "/portfolio/static/media/pigbank.18a2497c.PNG"
+  },
+  {
+    "revision": "9a4cb99151f61a04b5fd4d9d4634dff0",
+    "url": "/portfolio/index.html"
+  }
+];
